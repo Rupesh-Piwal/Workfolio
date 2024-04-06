bfdf4b6dd730e5d7053dc8637aaffaa4ead4b3ba
rupesh <rpiwal02@gmail.com>
2024-04-06 15:38:07 +0530

    created navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1 className="text-center text-3xl mt-6">Rupesh Bhai</h1>;
+  return <h1 className="text-center text-3xl mt-2">Rupesh Bhai</h1>;
 }
